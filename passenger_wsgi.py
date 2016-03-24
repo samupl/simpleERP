9fb401881c86893946908f99c278196f40f96cd3
Jakub Szafrański <s@samu.pl>
2016-03-24 07:49:10 +0100

    Passenger

---
+++ passenger_wsgi.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+os.environ['DJANGO_SETTINGS_MODULE'] = "simpleERP.settings"
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
