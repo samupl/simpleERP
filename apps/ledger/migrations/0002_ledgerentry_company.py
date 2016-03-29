c788694a2c3546393e2bf3813ac022b6473c17a1
Jakub Szafrański <s@samu.pl>
2016-03-29 19:33:57 +0200

    whoops

--- apps/ledger/migrations/0002_ledgerentry_company.py
+++ apps/ledger/migrations/0002_ledgerentry_company.py
@@ -9,7 +9,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0007_auto_20160329_1920'),
         ('ledger', '0001_initial'),
     ]
 
