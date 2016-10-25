614c279fa91a7e194efcb213fbaf4ad8826f0fd6
Jakub Szafrański <s@samu.pl>
2016-10-25 19:22:48 +0200

    Updated invoice template with logo

--- apps/invoices/urls.py
+++ apps/invoices/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^issue/(\d+)$', views.issue, name='issue'),
     url(r'^render/(\d+)$', views.render_invoice, name='render_invoice'),
-    url(r'^render/(\d+)/pdf$', views.render_invoice_pdf, name='render_invoice_pdf')
+    url(r'^render/(\d+)/pdf$', views.render_invoice_pdf, name='render_invoice_pdf'),
 ]
--- apps/invoices/views.py
+++ apps/invoices/views.py
@@ -8,6 +8,15 @@ from django.shortcuts import get_object_or_404, render
 from apps.invoices.models import Invoice
 
 
+pdfkit_options = {
+    'margin-top': '0px',
+    'margin-right': '0px',
+    'margin-bottom': '0px',
+    'margin-left': '0px',
+    'encoding': "UTF-8",
+}
+
+
 def render_invoice(request, invoice_id):
     invoice = get_object_or_404(Invoice, pk=invoice_id)
     return render(request, 'invoices/invoice.html', {
@@ -18,7 +27,9 @@ def render_invoice(request, invoice_id):
 
 def render_invoice_pdf(request, invoice_id):
     invoice = get_object_or_404(Invoice, pk=invoice_id)
-    pdf = pdfkit.from_string(invoice.rendered_html, False)
+    pdf = pdfkit.from_string(
+        invoice.rendered_html, False, options=pdfkit_options
+    )
     response = HttpResponse(content=pdf, content_type='application/pdf')
     return response
 
@@ -26,7 +37,11 @@ def render_invoice_pdf(request, invoice_id):
 def issue(request, invoice_id):
     invoice = get_object_or_404(Invoice, pk=invoice_id)
     file_name = invoice.generate_filename()
-    pdfkit.from_string(invoice.rendered_html, os.path.join(settings.MEDIA_ROOT, file_name))
+    pdfkit.from_string(
+        invoice.rendered_html,
+        os.path.join(settings.MEDIA_ROOT, file_name),
+        options=pdfkit_options
+    )
     invoice.invoice_pdf_file = file_name
     invoice.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
